fce044dff5815222d86972ecbcce8c0be8930be4
Carlos <mcarlos@tuta.io>
2024-05-08 15:01:11 -0700

    Updated about, toml & requirements

--- src/about.py
+++ src/about.py
@@ -2,7 +2,7 @@ import toml
 import versioningit
 
 data = toml.load("./pyproject.toml")
-print(data['project']['name'], end=" ")
+print(data['project']['title'], end=" ")
 
 version = versioningit.get_version(".",)
 print(version)
