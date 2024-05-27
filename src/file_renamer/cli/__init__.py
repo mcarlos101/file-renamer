331d941263b0a33f9fb1afafe36a69264ea287d9
Carlos <mcarlos@tuta.io>
2024-05-26 20:43:26 -0700

    Resized window to 1280x720

--- src/file_renamer/cli/__init__.py
+++ src/file_renamer/cli/__init__.py
@@ -6,6 +6,6 @@ from file_renamer import gui
 def start_app():
     app = QApplication(sys.argv)
     window = gui.MainWindow()
-    window.resize(800, 600)
+    window.resize(1280, 720)
     window.show()
     app.exec()
