bc9e5773051c7bce155e281e45b64df14fa80bd3
Carlos <mcarlos@tuta.io>
2024-05-31 07:27:57 -0700

    Minor text change

--- src/file_renamer/cli/__init__.py
+++ src/file_renamer/cli/__init__.py
@@ -6,7 +6,7 @@ from file_renamer import gui
 
 def start_app(**params):
     logging.info('__init.py__')
-    logging.info('%sstart_app', params['tab'])
+    logging.info('%sstart_app()', params['tab'])
     app = QApplication(sys.argv)
     window = gui.MainWindow(**params)
     window.resize(1280, 720)
