4251855a673e20923d4ea02b8a5b9098aa866b89
Carlos <mcarlos@tuta.io>
2024-06-01 23:40:04 -0700

    Fixed logs not rendering in whl installation

--- src/file_renamer/__main__.py
+++ src/file_renamer/__main__.py
@@ -1,51 +1,6 @@
 if __name__ == "__main__":
-    import os
-    import logging
-    import platform
-    import inspect
-    from pathlib import Path
+    # print ('__package__: ', __package__)
+    # print ('__name__   : ', __name__)
     from file_renamer.cli import start_app
 
-    tab = '   '  # 3 spaces
-    home = os.path.expanduser('~')
-
-    # Logs
-    logfile = Path(home + "/file-renamer.log")
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        filename=logfile,
-        encoding='utf-8',
-        filemode='w',
-        level=logging.DEBUG
-    )
-
-    try:
-        platform = platform.system()
-    except Exception as err:
-        print('See logs: ', logfile)
-        logger.exception(err)
-    else:
-        logger.info(__file__)
-        name = '__main__'
-        logger.info(name)
-        logger.info('platform: %s', platform)
-
-        fr = {
-            "home": home,
-            "platform": platform,
-            "widget": None,
-            "ui": None,
-            "path": "",
-            "base": "",
-            "dir": "",
-            "name": "",
-            "ext": "",
-            "id": "",
-            "new": "",
-            "current": "",
-            "html_title": "",
-            "html_body": ""
-        }
-
-        start_app(**fr)
+    start_app()
--- src/file_renamer/cli/__init__.py
+++ src/file_renamer/cli/__init__.py
@@ -1,15 +1,49 @@
 import logging
 import sys
+import os
+import platform
 import inspect
+from pathlib import Path
 from PySide6.QtWidgets import QApplication
 from file_renamer import gui
 
 
 def start_app(**fr):
-    logging.info("")
-    logging.info(__file__)
+    # Logs
+    home = os.path.expanduser('~')
+    logfile = Path(home + "/file-renamer.log")
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        filename=logfile,
+        encoding='utf-8',
+        filemode='w',
+        level=logging.DEBUG
+    )
+    logger.info(__file__)
     function = inspect.stack()[0].function
-    logging.info(function)
+    logger.info(function)
+    logger.info('platform: %s', platform.system())
+
+    # File Renamer dict
+    fr = {
+        "home": home,
+        "platform": platform,
+        "widget": None,
+        "ui": None,
+        "path": "",
+        "base": "",
+        "dir": "",
+        "name": "",
+        "ext": "",
+        "id": "",
+        "new": "",
+        "current": "",
+        "html_title": "",
+        "html_body": ""
+    }
+
+    # Launch app
     app = QApplication(sys.argv)
     window = gui.MainWindow(**fr)
     window.resize(1280, 720)
