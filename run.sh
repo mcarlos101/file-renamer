33d9d6cac07d4ea9018e5206293c899b75b38b57
Carlos <mcarlos@tuta.io>
2024-06-05 13:29:19 -0700

    Fixed local path to dev venv

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Run module file_renamer
-PYTHONPATH=src python -m file_renamer
+# Run module file_renamer from dev venv
+PYTHONPATH=src ../venv/dev/bin/python -m file_renamer
