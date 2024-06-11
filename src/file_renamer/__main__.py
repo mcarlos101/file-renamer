51ecd090a83b5a9a0d3bbe1aef70719e14af1ae5
Carlos <mcarlos@tuta.io>
2024-06-11 14:38:11 -0700

    Added Github Nuitka Actions

--- src/file_renamer/__main__.py
+++ src/file_renamer/__main__.py
@@ -1,3 +1,16 @@
+# Compilation mode, standalone everywhere, except on macOS there app bundle
+# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
+#    nuitka-project: --onefile
+# nuitka-project-if: {OS} == "Darwin":
+#    nuitka-project: --standalone
+#    nuitka-project: --macos-create-app-bundle
+#
+# Debugging options, controlled via environment variable at compile time.
+# nuitka-project-if: os.getenv("DEBUG_COMPILATION", "no") == "yes"
+#     nuitka-project: --enable-console
+# nuitka-project-else:
+#     nuitka-project: --disable-console
+
 if __name__ == "__main__":
     # print ('__package__: ', __package__)
     # print ('__name__   : ', __name__)
