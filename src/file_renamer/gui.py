7725a37181f81db17171afd010f87d073b6b065c
Carlos <mcarlos@tuta.io>
2024-06-17 15:02:34 -0700

    Ignored consecutive page reloads unless theme is changed

--- src/file_renamer/gui.py
+++ src/file_renamer/gui.py
@@ -103,13 +103,14 @@ class MainWindow(QMainWindow):
 
     def show_widget(self):
         logging.info(inspect.stack()[0].function)  # method name
-        if not self.dir_output:
-            self.dir_output.hide()
-        self.widget = Widget(**self.fr)
-        self.widget.show()
-        self.setCentralWidget(self.widget)
-        self.setWindowTitle(self.title)
-        self.fr['page-id'] = 'app'
+        if self.fr['page-id'] != 'app':
+            if not self.dir_output:
+                self.dir_output.hide()
+            self.widget = Widget(**self.fr)
+            self.widget.show()
+            self.setCentralWidget(self.widget)
+            self.setWindowTitle(self.title)
+            self.fr['page-id'] = 'app'
 
     def render_html(self):
         logging.info(inspect.stack()[0].function)  # method name
@@ -126,22 +127,23 @@ class MainWindow(QMainWindow):
     @Slot()
     def show_version(self):
         logging.info(inspect.stack()[0].function)  # method name
-        from file_renamer.version import __version__
-        title = "Version"
-        body = """
-        <div class="container">
-            <h1>File Renamer """ + __version__ + """</h1>
-            <p>GitHub<br>
-                https://github.com/mcarlos101/file-renamer/
-            </p>
-            <p>Python Package Index<br>
-                https://pypi.org/project/file-renamer/
-            </p>
-        </div>"""
-        self.fr['html_title'] = title
-        self.fr['html_body'] = body
-        self.fr['page-id'] = 'show_version'
-        self.render_html()
+        if self.fr['page-id'] != 'version':
+            from file_renamer.version import __version__
+            title = "Version"
+            body = """
+            <div class="container">
+                <h1>File Renamer """ + __version__ + """</h1>
+                <p>GitHub<br>
+                    https://github.com/mcarlos101/file-renamer/
+                </p>
+                <p>Python Package Index<br>
+                    https://pypi.org/project/file-renamer/
+                </p>
+            </div>"""
+            self.fr['html_title'] = title
+            self.fr['html_body'] = body
+            self.fr['page-id'] = 'version'
+            self.render_html()
 
     def set_theme(self):
         logging.info(inspect.stack()[0].function)  # method name
@@ -176,15 +178,20 @@ class MainWindow(QMainWindow):
         if qss:
             self.fr['app'].setStyleSheet(qss)
             logging.info('theme set: %s', self.fr['theme'])
-            if self.fr['page-id'] == 'show_version':
+            if self.fr['page-id'] == 'version':
+                self.fr['page-id'] = ""
                 self.show_version()
-            elif self.fr['page-id'] == 'show_license':
+            elif self.fr['page-id'] == 'license':
+                self.fr['page-id'] = ""
                 self.show_license()
-            elif self.fr['page-id'] == 'show_qt_for_python':
+            elif self.fr['page-id'] == 'qt-python':
+                self.fr['page-id'] = ""
                 self.show_qt_for_python()
-            elif self.fr['page-id'] == 'show_peace':
+            elif self.fr['page-id'] == 'peace':
+                self.fr['page-id'] = ""
                 self.show_peace()
-            elif self.fr['page-id'] == 'show_rbe':
+            elif self.fr['page-id'] == 'rbe':
+                self.fr['page-id'] = ""
                 self.show_rbe()
 
         else:
@@ -211,151 +218,155 @@ class MainWindow(QMainWindow):
     @Slot()
     def show_license(self):
         logging.info(inspect.stack()[0].function)  # method name
-        title = "License"
-        body = """
-        <div class="container">
-            <h1>Copyright (C) 2024  Carlos</h1>
-
-            <p>This program is free software: you can redistribute it and/or
-            modify it under the terms of the GNU General Public License as
-            published by the Free Software Foundation, either version 3 of the
-            License, or (at your option) any later version.</p>
-
-            <p>This program is distributed in the hope that it will be useful,
-            but WITHOUT ANY WARRANTY; without even the implied warranty of
-            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-            GNU General Public License for more details.</p>
-
-            <p>You should have received a copy of the GNU General Public
-            License along with this program.  If not, see
-            https://www.gnu.org/licenses/.</p>
-        </div>"""
-        self.fr['html_title'] = title
-        self.fr['html_body'] = body
-        self.fr['page-id'] = 'show_license'
-        self.render_html()
+        if self.fr['page-id'] != 'license':
+            title = "License"
+            body = """
+            <div class="container">
+                <h1>Copyright (C) 2024  Carlos</h1>
+    
+                <p>This program is free software: you can redistribute it and/or
+                modify it under the terms of the GNU General Public License as
+                published by the Free Software Foundation, either version 3 of the
+                License, or (at your option) any later version.</p>
+    
+                <p>This program is distributed in the hope that it will be useful,
+                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                GNU General Public License for more details.</p>
+    
+                <p>You should have received a copy of the GNU General Public
+                License along with this program.  If not, see
+                https://www.gnu.org/licenses/.</p>
+            </div>"""
+            self.fr['html_title'] = title
+            self.fr['html_body'] = body
+            self.fr['page-id'] = 'license'
+            self.render_html()
 
     @Slot()
     def show_qt_for_python(self):
         logging.info(inspect.stack()[0].function)  # method name
-        title = "Qt for Python"
-        body = """
-        <div class="container">
-            <h1>Qt for Python</h1>
-
-            <div class="p-3 text-primary-emphasis bg-primary-subtle border \
-                border-primary-subtle rounded-3">
-                Versions
-                <ul>
-                    <li>
-                        <strong>Qt</strong>:&nbsp;&nbsp;""" \
-                        + PySide6.QtCore.__version__ + """
-                    </li>
-                    <li><strong>PySide6</strong>:&nbsp;&nbsp;""" \
-                        + PySide6.__version__ + """
-                    </li>
-                </ul>
-            </div>
-
-            <p>Qt for Python offers the official Python bindings for Qt, which
-            enables you to use Python to write your Qt applications. The
-            project has two main components:</p>
-                <ol>
-                    <li>PySide6, so that you can use Qt6 APIs in your Python
-                    applications, and</li>
-
-                    <li>Shiboken6, a binding generator tool, which can be used
-                    to expose C++ projects to Python, and a Python module with
-                    some utility functions.</li>
-                </ol>
-            <p>This project is available under the LGPLv3/GPLv3 and the Qt
-            commercial license.</p>
-
-            <p>Qt for Python<br>
-            https://www.qt.io/qt-for-python</p>
-
-            <p>Docs<br>
-            https://doc.qt.io/qtforpython-6/</p>
-        </div>"""
-        self.fr['html_title'] = title
-        self.fr['html_body'] = body
-        self.fr['page-id'] = 'show_qt_for_python'
-        self.render_html()
+        if self.fr['page-id'] != 'qt-python':
+            title = "Qt for Python"
+            body = """
+            <div class="container">
+                <h1>Qt for Python</h1>
+    
+                <div class="p-3 text-primary-emphasis bg-primary-subtle border \
+                    border-primary-subtle rounded-3">
+                    Versions
+                    <ul>
+                        <li>
+                            <strong>Qt</strong>:&nbsp;&nbsp;""" \
+                            + PySide6.QtCore.__version__ + """
+                        </li>
+                        <li><strong>PySide6</strong>:&nbsp;&nbsp;""" \
+                            + PySide6.__version__ + """
+                        </li>
+                    </ul>
+                </div>
+    
+                <p>Qt for Python offers the official Python bindings for Qt, which
+                enables you to use Python to write your Qt applications. The
+                project has two main components:</p>
+                    <ol>
+                        <li>PySide6, so that you can use Qt6 APIs in your Python
+                        applications, and</li>
+    
+                        <li>Shiboken6, a binding generator tool, which can be used
+                        to expose C++ projects to Python, and a Python module with
+                        some utility functions.</li>
+                    </ol>
+                <p>This project is available under the LGPLv3/GPLv3 and the Qt
+                commercial license.</p>
+    
+                <p>Qt for Python<br>
+                https://www.qt.io/qt-for-python</p>
+    
+                <p>Docs<br>
+                https://doc.qt.io/qtforpython-6/</p>
+            </div>"""
+            self.fr['html_title'] = title
+            self.fr['html_body'] = body
+            self.fr['page-id'] = 'qt-python'
+            self.render_html()
 
     @Slot()
     def show_peace(self):
         logging.info(inspect.stack()[0].function)  # method name
-        import file_renamer.html.rc_imagine
-        title = "Imagine Peace On Earth"
-        body = """
-        <div class="container text-center">
-            <div class="row">
-                <h1>""" + title + """</h1>
-                <video controls autoplay>
-                  <source src='qrc:/imagine_mp4' type="video/mp4" codecs="vp9">
-                Your browser does not support the video tag.
-                </video>
+        if self.fr['page-id'] != 'peace':
+            import file_renamer.html.rc_imagine
+            title = "Imagine Peace On Earth"
+            body = """
+            <div class="container text-center">
+                <div class="row">
+                    <h1>""" + title + """</h1>
+                    <video controls autoplay>
+                      <source src='qrc:/imagine_mp4' type="video/mp4" codecs="vp9">
+                    Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
-        </div>
-        <div class="container">
-            <h2>Credits</h2>
-            <p>
-                Song: John Lennon - Imagine (1971)<br>
-                Background Image: https://pngkit.com</p>
-            <p>
-                Copyright Disclaimer under Section 107 of the Copyright Act of
-                1976: Allowance is made for "fair use" for purposes such as
-                criticism, comment, news reporting, teaching, scholarship,
-                education, and research.<br>
-                Fair use is a use permitted by copyright statute that might
-                otherwise be infringing.<br>
-                All rights and credit go directly to its rightful owners. No
-                copyright infringement is intended.
-            </p>
-        </div>
-"""
-        self.fr['html_title'] = title
-        self.fr['html_body'] = body
-        self.fr['page-id'] = 'show_peace'
-        self.render_html()
+            <div class="container">
+                <h2>Credits</h2>
+                <p>
+                    Song: John Lennon - Imagine (1971)<br>
+                    Background Image: https://pngkit.com</p>
+                <p>
+                    Copyright Disclaimer under Section 107 of the Copyright Act of
+                    1976: Allowance is made for "fair use" for purposes such as
+                    criticism, comment, news reporting, teaching, scholarship,
+                    education, and research.<br>
+                    Fair use is a use permitted by copyright statute that might
+                    otherwise be infringing.<br>
+                    All rights and credit go directly to its rightful owners. No
+                    copyright infringement is intended.
+                </p>
+            </div>
+    """
+            self.fr['html_title'] = title
+            self.fr['html_body'] = body
+            self.fr['page-id'] = 'peace'
+            self.render_html()
 
     @Slot()
     def show_rbe(self):
         logging.info(inspect.stack()[0].function)  # method name
-        import file_renamer.html.rc_rbe
-        title = "Resource Based Economy"
-        body = """
-        <div class="container text-center">
-            <div class="row">
-                <h1>""" + title + """</h1>
-                <video controls autoplay>
-                  <source src='qrc:/venus_project_mp4' type="video/mp4"
-                  codecs="vp9">
-                Your browser does not support the video tag.
-                </video>
+        if self.fr['page-id'] != 'rbe':
+            import file_renamer.html.rc_rbe
+            title = "Resource Based Economy"
+            body = """
+            <div class="container text-center">
+                <div class="row">
+                    <h1>""" + title + """</h1>
+                    <video controls autoplay>
+                      <source src='qrc:/venus_project_mp4' type="video/mp4"
+                      codecs="vp9">
+                    Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
-        </div>
-        <div class="container">
-            <h2>Credits</h2>
-            <p>
-                Song: The Lost Children Of Babylon -  The Venus Project
-                (2010)<br>
-                https://lostchildrenofbabylon.com/</p>
-            <p>
-                Background Image: https://www.resourcebasedeconomy.org
-            </p>
-            <p>
-                Copyright Disclaimer under Section 107 of the Copyright Act of
-                1976: Allowance is made for "fair use" for purposes such as
-                criticism, comment, news reporting, teaching, scholarship,
-                education, and research.<br>
-                Fair use is a use permitted by copyright statute that might
-                otherwise be infringing.<br>
-                All rights and credit go directly to its rightful owners. No
-                copyright infringement is intended.
-            </p>
-        </div>"""
-        self.fr['html_title'] = title
-        self.fr['html_body'] = body
-        self.fr['page-id'] = 'show_rbe'
-        self.render_html()
+            <div class="container">
+                <h2>Credits</h2>
+                <p>
+                    Song: The Lost Children Of Babylon -  The Venus Project
+                    (2010)<br>
+                    https://lostchildrenofbabylon.com/</p>
+                <p>
+                    Background Image: https://www.resourcebasedeconomy.org
+                </p>
+                <p>
+                    Copyright Disclaimer under Section 107 of the Copyright Act of
+                    1976: Allowance is made for "fair use" for purposes such as
+                    criticism, comment, news reporting, teaching, scholarship,
+                    education, and research.<br>
+                    Fair use is a use permitted by copyright statute that might
+                    otherwise be infringing.<br>
+                    All rights and credit go directly to its rightful owners. No
+                    copyright infringement is intended.
+                </p>
+            </div>"""
+            self.fr['html_title'] = title
+            self.fr['html_body'] = body
+            self.fr['page-id'] = 'rbe'
+            self.render_html()
