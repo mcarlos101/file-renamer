c653f01bd70e757a478e340e4d7280a604e1000b
Carlos <mcarlos@tuta.io>
2024-10-13 10:20:42 -0700

    Changed from vox to venv with full path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Run module file_renamer from dev venv
-PYTHONPATH=src ../vox/dev/bin/python -m file_renamer
+# Run module file_renamer from venv dev
+PYTHONPATH=/data/fr/file-renamer/src /data/fr/venv/dev/bin/python -m file_renamer
