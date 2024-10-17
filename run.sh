45f44aca86c5dcc9fce31386d6979836929b7a2b
Carlos <mcarlos@tuta.io>
2024-10-16 19:21:41 -0700

    Updated path to venv dev

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Run module file_renamer from venv dev
-PYTHONPATH=/data/fr/file-renamer/src /data/fr/venv/dev/bin/python -m file_renamer
+PYTHONPATH=/data/fr/file-renamer/src \
+/data/fr/venv/dev-py3.11/bin/python -m file_renamer
