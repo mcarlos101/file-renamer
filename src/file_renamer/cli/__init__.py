9bf76fa349c5d01060492ffb35c1baa6f9ef9a39
Carlos <mcarlos@tuta.io>
2024-11-19 17:43:12 -0800

    Removed screen resolution detection

--- src/file_renamer/cli/__init__.py
+++ src/file_renamer/cli/__init__.py
@@ -63,44 +63,32 @@ def start_app(**fr):
     width = 800
     height = 600
 
-    try:
-        # Detect screen resolution
-        detected_width, detected_height = app.primaryScreen().size().toTuple()
-        logger.info('detected_width: %s', detected_width)
-        logger.info('detected_height: %s', detected_height)
-        if detected_width > width and detected_height > height:
-            width = detected_width
-            height = detected_height
-    except Exception as err:
-        print('See logs: ', logfile)
-        logger.exception(err)
-    else:
-        window = MainWindow(**fr)
-        window.resize(width, height)
-        window.show()
+    window = MainWindow(**fr)
+    window.resize(width, height)
+    window.show()
 
-        # Set theme
-        qss = ""
-        if fr["platform"] == "Windows":
-            if fr['theme'] == 'light':
-                from file_renamer.themes.light.light_windows import (
-                    LightWindows
-                )
-                style = LightWindows()
-                qss = style.theme
-            elif fr['theme'] == 'dark':
-                from file_renamer.themes.dark.dark_windows import DarkWindows
-                style = DarkWindows()
-                qss = style.theme
-        else:
-            if fr['theme'] == 'light':
-                from file_renamer.themes.light.light_linux import LightLinux
-                style = LightLinux()
-                qss = style.theme
-            elif fr['theme'] == 'dark':
-                from file_renamer.themes.dark.dark_linux import DarkLinux
-                style = DarkLinux()
-                qss = style.theme
-        app.setStyleSheet(qss)
-        logger.info('theme set: %s', fr['theme'])
-        app.exec()
+    # Set theme
+    qss = ""
+    if fr["platform"] == "Windows":
+        if fr['theme'] == 'light':
+            from file_renamer.themes.light.light_windows import (
+                LightWindows
+            )
+            style = LightWindows()
+            qss = style.theme
+        elif fr['theme'] == 'dark':
+            from file_renamer.themes.dark.dark_windows import DarkWindows
+            style = DarkWindows()
+            qss = style.theme
+    else:
+        if fr['theme'] == 'light':
+            from file_renamer.themes.light.light_linux import LightLinux
+            style = LightLinux()
+            qss = style.theme
+        elif fr['theme'] == 'dark':
+            from file_renamer.themes.dark.dark_linux import DarkLinux
+            style = DarkLinux()
+            qss = style.theme
+    app.setStyleSheet(qss)
+    logger.info('theme set: %s', fr['theme'])
+    app.exec()
